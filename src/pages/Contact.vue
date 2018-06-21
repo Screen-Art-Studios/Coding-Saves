caaf0e911c5002c840e25a6a72747f7d014e540c
Avery Jordan Angel <kazy297@hotmail.com>
2018-06-21 15:14:45 -0700

    maybe something'll work

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,31 +2,66 @@
   <div class="main">
     <div class="colorback"></div>
     <h1>Send Us A Message</h1>
-    <input v-modal ="" class="name" placeholder="Name" required>
-    <input v-modal ="" class="email" placeholder="Email" required>
-    <input v-modal ="" class="phone" placeholder="Phone">
-    <textarea v-modal ="" class="message" placeholder="Message" required></textarea>
-    <button class="send">SEND</button>
+    <input v-model ="name" class="name" placeholder="Name" required>
+    <input v-model ="email" class="email" placeholder="Email" required>
+    <input v-model ="phone" class="phone" placeholder="Phone">
+    <textarea v-model ="message" class="message" placeholder="Message" required></textarea>
+    <button class="send" v-on:click="submitMessage">SEND</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'contact'
+  name: 'contact',
+  data: function () {
+    return {
+      success: false,
+      error: false,
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
+    }
+  },
+  methods: {
+    submitMessage () {
+      let vue = this
+      axios.post('https://52.53.126.148:81/email', {
+        name: vue.name.toLowerCase(),
+        phone: vue.phone,
+        email: vue.email,
+        message: vue.message
+      })
+        .then(function (message) {
+          vue.success = true
+          console.log(message)
+          console.log(vue.leadId)
+        })
+        .catch(function (error) {
+          console.log(error)
+          vue.error = true
+        })
+    }
+  }
 }
 </script>
 
 <style scoped>
+
 .main {
   position: fixed;
   width: 100%;
   height: 100%;
 }
+
 .colorback {
   width: 100%;
   height: 140px;
   background-image: url('../assets/noise.png');
 }
+
 input {
   border: 1px solid black;
   width: 90%;
@@ -35,6 +70,7 @@ input {
   margin-bottom: 2%;
   padding-left: 2%;
 }
+
 textarea {
   border: 1px solid black;
   width: 90%;
@@ -42,6 +78,7 @@ textarea {
   margin-left: 5%;
   padding-left: 2%;
 }
+
 button {
   height:50px;
   color: white;
@@ -54,6 +91,7 @@ button {
   margin-right: 5%;
   font-size: 2.5em;
 }
+
 h1 {
   text-align: center;
   font-size: 2em;
